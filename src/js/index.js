0dcf3994d770b770e0c14876499948d0687ede38
Danver <danver.ver@hotmail.com>
2024-10-14 16:29:12 -0300

    Adição das funcionalidades da API e busca de cidades

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,47 @@
-const keyAPI = "80b5e4352e6a4109835183057241410";
+const keyAPI = "PUT_YOUR_KEY"; // https://www.weatherapi.com/signup.aspx
+const baseURL = "http://api.weatherapi.com/v1"
 const language = "pt"
 
 const searchButton = document.querySelector(".search-button");
 
-searchButton.addEventListener("click", async () =>{
+searchButton.addEventListener("click", async () => {
     const city = document.getElementById("search-input").value;
 
     const data = await searchCityData(city);
 
-    console.log(data);
-    
+    console.log(data)
+
+    fillData(data, city);
+
 });
 
 
-async function searchCityData(city){
-    const apiURL = `http://api.weatherapi.com/v1/current.json?key=${keyAPI}&q=${city}&aqi=no&lang=${language}`;
+async function searchCityData(city) {
+    const apiURL = `${baseURL}/current.json?key=${keyAPI}&q=${city}&aqi=no&lang=${language}`;
 
     const response = await fetch(apiURL);
 
     const data = response.json();
 
     return data;
+}
+
+function fillData(data, city) {
+    const temperature = data.current.temp_c
+    const condition = data.current.condition.text;
+    const humidity = data.current.humidity
+    const windSpeed = data.current.wind_kph
+    const conditionIcon = data.current.condition.icon
+
+    document.getElementById("city").textContent = city
+
+    document.getElementById("temperature").textContent = `${temperature.toFixed(0)} °C`
+
+    document.getElementById("climate-condition").textContent = condition
+
+    document.getElementById("humidity").textContent = `${humidity}%`
+
+    document.getElementById("wind-speed").textContent = `${windSpeed} km/h`
+
+    document.getElementById("climate-icon").setAttribute("src", conditionIcon)
 }
